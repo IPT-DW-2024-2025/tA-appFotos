663f75dac59996041784dc814dca8ad0c8cfd9d7
jcnpereira <casimiro@ipt.pt>
2025-05-13 11:48:12 +0100

    Acesso a dados do utilizador cuja JWT é enviada à API

---
+++ AppFotos/AppFotos/Models/ViewModels/FotografiaDTObyUser.cs
@@ -0,0 +1,32 @@
+﻿namespace AppFotos.Models.ViewModels {
+   public class FotografiaDTObyUser {
+
+      /// <summary>
+      /// título da fotografia
+      /// </summary>
+      public string Titulo { get; set; } = "";
+
+      /// <summary>
+      /// Descrição da fotografia
+      /// </summary>
+      public string? Descricao { get; set; }
+
+      /// <summary>
+      /// Nome do ficheiro da fotografia no disco rígido
+      /// do servidor
+      /// </summary>
+      public string Ficheiro { get; set; } = null!;
+
+      /// <summary>
+      /// Data em que a fotografia foi tirada
+      /// </summary>
+      public DateTime Data { get; set; }
+
+      /// <summary>
+      /// Nome do dono da fotografia
+      /// </summary>
+      public string NomeFotografo { get; set; } = "";
+
+
+    }
+}
