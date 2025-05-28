1ac3cb557923b2a35f77432b2c6564ddc9bd6263
jcnpereira <casimiro@ipt.pt>
2025-05-28 10:47:15 +0100

    Adição do controller API Fotografias, com restrição de acesso a dados das fotografias

--- AppFotos/AppFotos/Controllers/API/FotografiasController.cs
+++ AppFotos/AppFotos/Controllers/API/FotografiasController.cs
@@ -7,102 +7,112 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AppFotos.Data;
 using AppFotos.Models;
+using AppFotos.Models.ViewModels;
 
-namespace AppFotos.Controllers.API
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FotografiasController : ControllerBase
-    {
-        private readonly ApplicationDbContext _context;
+namespace AppFotos.Controllers.API {
+   [Route("api/[controller]")]
+   [ApiController]
+   public class FotografiasController:ControllerBase {
+      private readonly ApplicationDbContext _context;
 
-        public FotografiasController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+      public FotografiasController(ApplicationDbContext context) {
+         _context=context;
+      }
 
-        // GET: api/Fotografias
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Fotografias>>> GetFotografias()
-        {
-            return await _context.Fotografias.ToListAsync();
-        }
+      // GET: api/Fotografias
+      [HttpGet]
+      public async Task<ActionResult<IEnumerable<FotografiasDTO>>> GetFotografias() {
+         // o que tínhamos
+         // // SELECT *
+         // // FROM Fotografias
+         // return await _context.Fotografias.ToListAsync();
 
-        // GET: api/Fotografias/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Fotografias>> GetFotografia(int id)
-        {
-            var fotografia = await _context.Fotografias.FindAsync(id);
+         // o que pretendemos...
+         // SELECT Titulo, Descricao, Data, Ficheiro
+         // FROM Fotografias
 
-            if (fotografia == null)
-            {
-                return NotFound();
-            }
+         var listagemFotos = await _context.Fotografias
+                                           .OrderByDescending(f => f.Data)
+                                           .Select(f => new FotografiasDTO {
+                                              Titulo=f.Titulo,
+                                              Descricao=f.Descricao,
+                                              Ficheiro=f.Ficheiro,
+                                              Data=f.Data
+                                           })
+                                           .ToListAsync();
+         return listagemFotos;
+      }
 
-            return fotografia;
-        }
+      // GET: api/Fotografias/5
+      [HttpGet("{id}")]
+      public async Task<ActionResult<FotografiasDTO>> GetFotografia(int id) {
+         var fotografia = await _context.Fotografias
+                                        .Where(f => f.Id==id)
+                                        .Select(f => new FotografiasDTO {
+                                           Titulo=f.Titulo,
+                                           Ficheiro=f.Ficheiro,
+                                           Data=f.Data
+                                        })
+                                        .FirstOrDefaultAsync();
 
-        // PUT: api/Fotografias/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutFotografia(int id, Fotografias fotografia)
-        {
-            if (id != fotografia.Id)
-            {
-                return BadRequest();
-            }
+         if (fotografia==null) {
+            return NotFound();
+         }
 
-            _context.Entry(fotografia).State = EntityState.Modified;
+         return fotografia;
+      }
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!FotografiaExiste(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+      // PUT: api/Fotografias/5
+      // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPut("{id}")]
+      public async Task<IActionResult> PutFotografia(int id,Fotografias fotografia) {
+         if (id!=fotografia.Id) {
+            return BadRequest();
+         }
 
-            return NoContent();
-        }
+         _context.Entry(fotografia).State=EntityState.Modified;
 
-        // POST: api/Fotografias
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Fotografias>> PostFotografia(Fotografias fotografia)
-        {
-            _context.Fotografias.Add(fotografia);
+         try {
             await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetFotografias", new { id = fotografia.Id }, fotografia);
-        }
-
-        // DELETE: api/Fotografias/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteFotografia(int id)
-        {
-            var fotografia = await _context.Fotografias.FindAsync(id);
-            if (fotografia == null)
-            {
-                return NotFound();
+         }
+         catch (DbUpdateConcurrencyException) {
+            if (!FotografiaExiste(id)) {
+               return NotFound();
             }
+            else {
+               throw;
+            }
+         }
 
-            _context.Fotografias.Remove(fotografia);
-            await _context.SaveChangesAsync();
+         return NoContent();
+      }
 
-            return NoContent();
-        }
+      // POST: api/Fotografias
+      // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPost]
+      public async Task<ActionResult<Fotografias>> PostFotografia(Fotografias fotografia) {
+         _context.Fotografias.Add(fotografia);
+         await _context.SaveChangesAsync();
 
-        private bool FotografiaExiste(int id)
-        {
-            return _context.Fotografias.Any(e => e.Id == id);
-        }
-    }
+         return CreatedAtAction("GetFotografias",new { id = fotografia.Id },fotografia);
+      }
+
+      // DELETE: api/Fotografias/5
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> DeleteFotografia(int id) {
+         var fotografia = await _context.Fotografias.FindAsync(id);
+         if (fotografia==null) {
+            return NotFound();
+         }
+
+         _context.Fotografias.Remove(fotografia);
+         await _context.SaveChangesAsync();
+
+         return NoContent();
+      }
+
+      private bool FotografiaExiste(int id) {
+         return _context.Fotografias.Any(e => e.Id==id);
+      }
+   }
 }
